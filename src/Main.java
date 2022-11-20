60019fa6572ec17143d85d422f64d8ea69724029
hongxeob <sjun2918@naver.com>
2022-11-20 14:37:59 +0900

    Feat : 플레이어&컴퓨터 같은 숫자 갯수 파악 기능

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import Model.Judgement;
 import Model.NumberGenerator;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        final NumberGenerator generator = new NumberGenerator();
-        List<Integer> numberList = generator.createRandomNumbers();
-        System.out.println(numberList);
+//        final NumberGenerator generator = new NumberGenerator();
+//        List<Integer> numberList = generator.createRandomNumbers();
+//        System.out.println(numberList);
+
+        Judgement judgement = new Judgement();
+        final int count = judgement.correctCount(Arrays.asList(1, 2, 3), Arrays.asList(3, 4, 5));
+        System.out.println(count);
+
     }
 
 }
