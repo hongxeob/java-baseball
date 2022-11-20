75bb61b7f5b18a20b818d28542886f5ad3f9756a
hongxeob <sjun2918@naver.com>
2022-11-20 17:47:35 +0900

    error 발견 IndexOutOfBoundsException

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import baseball.Model.NumberGenerator;
 import baseball.Model.Refree;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,9 +20,35 @@ public class Main {
 //        final boolean place = judgement.hasPlace(Arrays.asList(1, 2, 3), 1, 2);
 //        System.out.println(place);
 
+//        final Refree refree = new Refree();
+//        final String result = refree.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 2, 1));
+//        System.out.println(result);
+
+
+        final NumberGenerator generator = new NumberGenerator();
+        final List<Integer> computer = generator.createRandomNumbers();
+
         final Refree refree = new Refree();
-        final String result = refree.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 2, 1));
-        System.out.println(result);
+
+        String reslut = "";
+        while (!reslut.equals("정답입니다.")) {
+            reslut = refree.compare(computer, askNumbers());
+            System.out.println(reslut);
+        }
+        System.out.println("3개의 숫자를 모두 맞췄습니다! 게임 종료");
+
+    }
+
+    public static List<Integer> askNumbers() {
+        System.out.println("숫자를 입력하세요 : ");
+        final Scanner sc = new Scanner(System.in);
+        final String input = sc.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 
 }
--- src/main/java/baseball/Model/Judgement.java
+++ src/main/java/baseball/Model/Judgement.java
@@ -7,8 +7,8 @@ public class Judgement {
         int result = 0; // 3
         for (int i = 0; i < computer.size(); i++) { // 컴퓨터에 담겨있는 size만큼 반복
             int playerNumber = player.get(i); // playerNumber는 매개변수 player에 i가 들어간 것
-                                            // player가 돌면서 computer에게 묻는다. 사용자가 입력한 값이 컴퓨터가 갖고 있는지
-            computer.contains(playerNumber); // List의 contains() 메소드는, List안에 같은 값이 있으면  true를, 없으면 false를 리턴합니다.
+            computer.contains(playerNumber); // player가 돌면서 computer에게 묻는다. 사용자가 입력한 값이 컴퓨터가 갖고 있는지
+            // List의 contains() 메소드는, List안에 같은 값이 있으면  true를, 없으면 false를 리턴합니다.
 
             if (computer.contains(playerNumber)) { // 갖고 있다면
                 result++;
