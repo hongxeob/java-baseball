d18b85a4046df86ea4f651bc986992e6df8a50c5
hongxeob <sjun2918@naver.com>
2022-11-20 14:03:16 +0900

    Feat : 추가 정답이될 랜덤 숫자 생성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import Model.NumberGenerator;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final NumberGenerator generator = new NumberGenerator();
+        List<Integer> numberList = generator.createRandomNumbers();
+        System.out.println(numberList);
     }
+
 }
--- src/Model/NumberGenerator.java
+++ src/Model/NumberGenerator.java
@@ -1,9 +1,19 @@
 package Model;
 
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class NumberGenerator {
-    public List<String> createRandomNumbers() { // 랜덤 숫자 3개를 생성하는 메소드
-        return null;
+public class NumberGenerator { // 1.클래스 생성
+    public List<Integer> createRandomNumbers() { // 2.정답이 될 랜덤 숫자 3개를 생성하는 메서드
+        List<Integer> numberList = new ArrayList<>(); // 숫자들을 담을 저장(collection)
+        for (int i = 0; i < 3; i++) { // 3번 반복 한다 ->
+            int number = (int) (Math.random() * 9 + 1); // 1~9까지의 랜덤 수를
+            numberList.add(number); // 위의 숫자 담는 저장소에 저장
+        }
+        return numberList;
+
+        // Main함수에서 실행시켜 보기
     }
 }
